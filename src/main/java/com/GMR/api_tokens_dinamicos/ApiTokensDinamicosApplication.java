a228e5e2b2c7d53b7d6890c8e20a368803d68c41
Reality <machado.renato@outlook.com>
2026-04-12 23:41:11 -0300

    Commit inicial: estrutura do projeto com Spring Boot (vanilla)

---
+++ src/main/java/com/GMR/api_tokens_dinamicos/ApiTokensDinamicosApplication.java
@@ -0,0 +1,13 @@
+package com.GMR.api_tokens_dinamicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTokensDinamicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTokensDinamicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GMR/api_tokens_dinamicos/ApiTokensDinamicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GMR.api_tokens_dinamicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTokensDinamicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
